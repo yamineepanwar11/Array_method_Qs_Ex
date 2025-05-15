39ddd31d2ff1e62570a4e2dbb593b07e82bbb9ce
Yaminee Panwar <yamineepanwar998@gmail.com>
2025-05-15 14:44:25 +0530

    initial commit

--- Qs10.js
+++ Qs10.js
@@ -1,12 +1,29 @@
+// Finding the frequency of elements in an array :
+
+// let arr = [1, 2, 3, 4, 5, 1, 2];
+
+// let final = arr.reduce((prev, curr) => {
+//     if (prev[curr]) {
+//         prev[curr]++;
+//     } else {
+//         prev[curr] = 1;
+//     }
+//     return prev;
+// }, {});
+
+// console.log(final);
+
+
+
+//2nd method 
 let arr = [1, 2, 3, 4, 5, 1, 2];
-
-let final = arr.reduce((prev, curr) => {
-    if (prev[curr]) {
-        prev[curr]++;
-    } else {
-        prev[curr] = 1;
+for(let i=0;i<=arr.length-1;i++){
+    let count=0;
+    for (let j=0;j<=arr.length-1;j++){
+        if(arr[i]==arr[j]){
+            count++;
+        }
     }
-    return prev;
-}, {});
+    console.log(arr[i]+"=>",count);
+}
 
-console.log(final);
