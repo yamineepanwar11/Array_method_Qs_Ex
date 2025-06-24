f3904c7723b7181586e68fe318b4f38d449ab363
Yaminee Panwar <yamineepanwar998@gmail.com>
2025-06-25 00:26:59 +0530

    initial commit

--- pattern.js
+++ pattern.js
@@ -1,11 +1,17 @@
+// let n=5;
+// for(let i=1;i<=n;i++){
+//     let str='';
+//     for(let j=1;j<=i;j++){
+//          str+='*';
+//     }
+//     console.log(str);
+// }
+
 let n=5;
 for(let i=1;i<=n;i++){
-    let row="";
-    for(let j=1;j<n-i+1;j++){
-        row=row+" ";
+    let str='';
+    for(let j=1;j<=n-i+1;j++){
+         str+='*';
     }
-    for(let k=1;k<=i;k++){
-        row=row+"*";
-    }
-    console.log(row);
+    console.log(str);
 }
