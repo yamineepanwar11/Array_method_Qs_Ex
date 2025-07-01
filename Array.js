28946cc20135c7388e2caaa323b6f400dba83bd8
Yaminee Panwar <yamineepanwar998@gmail.com>
2025-07-01 16:25:15 +0530

    initial commit

--- Array.js
+++ Array.js
@@ -5,5 +5,16 @@ let reversed = [];
 for (let i = arr.length - 1; i >= 0; i--) {
   reversed.push(arr[i]);
 }
-
 console.log(reversed); 
+
+//2nd method
+let arr1=[1, 2, 3, 4, 5,6,7,8,9];
+let temp=[];
+let index=0;
+for (let i=arr1.length-1; i>=0; i--){
+    temp[index]=arr1[i];
+    index++;
+}
+console.log(temp);
+
+//
