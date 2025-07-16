074c8ffdc3afd5eacf89a23b249aeb43651a6f32
Yaminee Panwar <yamineepanwar998@gmail.com>
2025-07-17 01:28:26 +0530

    initial commit

--- array5.js
+++ array5.js
@@ -10,19 +10,19 @@
 // console.log(result);
 
 //Move All Zeroes To End
-let arr=[1,2,0,45,221,0,78,0];
-let result=[];
+// let arr=[1,2,0,45,221,0,78,0];
+// let result=[];
 
-for(let i=0;i<=arr.length-1;i++){
-    if(arr[i]!==0){
-        result.push(arr[i])
-    }
+// for(let i=0;i<=arr.length-1;i++){
+//     if(arr[i]!==0){
+//         result.push(arr[i])
+//     }
   
-}
-  while(result.length<arr.length){
-        result.push(0);
-    }
-console.log(result);
+// }
+//   while(result.length<arr.length){
+//         result.push(0);
+//     }
+// console.log(result);
 
 
 //Reverse an Array Without Using Built-in Methods
@@ -34,3 +34,35 @@ console.log(result);
 //     index++;
 // }
 // console.log(temp);
+
+
+
+
+//Even and odd numbers sum
+let arr=[1,2,4,3,5,6,7,8,9];
+let evensum=0;
+let oddsum=0;
+for(let i=0;i<=arr.length-1;i++){
+    if(arr[i]%2===0){
+        evensum+=arr[i];
+    }
+    else{
+        oddsum+=arr[i];
+    }
+}
+console.log(evensum);
+console.log(oddsum);
+
+
+//Same elememts of both array
+let arr1=[1,2,3,4,0];
+let arr2=[1,5,4,7,0,2];
+let match=[];
+for(let i=0;i<=arr1.length-1;i++){
+    for(let j=0;j<=arr2.length-1;j++){
+        if(arr1[i]===arr2[j]){
+          match.push(arr1[i])  
+        }
+    }
+}
+console.log(match);
