6037cdef1d525ea5c95c46a81ef41953b14d3a7d
Yaminee Panwar <yamineepanwar998@gmail.com>
2025-07-18 01:37:37 +0530

    initial commit

--- array5.js
+++ array5.js
@@ -39,19 +39,19 @@
 
 
 //Even and odd numbers sum
-let arr=[1,2,4,3,5,6,7,8,9];
-let evensum=0;
-let oddsum=0;
-for(let i=0;i<=arr.length-1;i++){
-    if(arr[i]%2===0){
-        evensum+=arr[i];
-    }
-    else{
-        oddsum+=arr[i];
-    }
-}
-console.log(evensum);
-console.log(oddsum);
+// let arr=[1,2,4,3,5,6,7,8,9];
+// let evensum=0;
+// let oddsum=0;
+// for(let i=0;i<=arr.length-1;i++){
+//     if(arr[i]%2===0){
+//         evensum+=arr[i];
+//     }
+//     else{
+//         oddsum+=arr[i];
+//     }
+// }
+// console.log(evensum);
+// console.log(oddsum);
 
 
 //Same elememts of both array
@@ -66,3 +66,78 @@ for(let i=0;i<=arr1.length-1;i++){
     }
 }
 console.log(match);
+
+
+// 1: find the frequency of any specific number.
+let arr=[1,2,3,1,2,42,1];
+let num=1;
+let count=0;
+for(let i=0;i<arr.length;i++){
+    if(arr[i]==num){
+        count++;
+    }
+}
+
+console.log(count);
+
+
+//2:find prime
+// let arr=[4,2,3,5,4,6,7,8,13,9,17,19,21];
+// let count=0;
+// let prime=[];
+// for(let i=0;i<arr.length;i++){
+//     let count=0
+//     for(let j=2;j<arr[i];j++){
+//         if(arr[i]%j==0){
+//             count++;
+//         }
+//     }
+//     if(count==0 && arr[i]!=1){
+//         prime.push(arr[i])
+//     }
+// }
+// console.log(prime)
+
+
+//3.replce each element with product of the other
+
+// let arr=[1,2,3,4];
+// let dup=[];
+// for(let i=0;i<arr.length;i++){
+//     let mul=1;
+//     for(let j=0;j<arr.length;j++){
+//         if(arr[i]!=arr[j]){
+//         mul*=arr[j]
+//     }
+//     }
+//     dup.push(mul)
+// }
+// console.log(dup)
+
+
+//4.longest word in array of string
+
+// let arr=["apple","banana","oraange","mango","lichi"];
+// let len=0;
+// let data=""
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i].length>len){
+//         len=arr[i].length
+//         data=arr[i]
+//     }
+    
+// }
+// console.log(data)
+
+
+
+let a = [1, 2, 2, 3, 4, 4, 5];
+let unique = [];
+
+for (let i = 0; i < a.length; i++) {
+    if (!unique.includes(a[i])) {
+        unique.push(a[i]);
+    }
+}
+
+console.log(unique); 
