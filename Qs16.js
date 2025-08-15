d28663c76305ee6bbf87753ebf8401c23aa9b9cb
Yaminee Panwar <yamineepanwar998@gmail.com>
2025-08-15 14:58:22 +0530

    Initial commit

---
+++ Qs16.js
@@ -0,0 +1,14 @@
+let n=5;
+for(let i=1;i<=n;i++){
+   let str=""
+    for(let j=1;j<=n;j++){
+        str+=i*j+" ";
+    }
+    console.log(str);
+}
+//OUTPUT
+//1 2 3 4 5 
+// 2 4 6 8 10 
+// 3 6 9 12 15 
+// 4 8 12 16 20 
+// 5 10 15 20 25 
